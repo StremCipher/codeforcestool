ef1504841ce6c1fe2f224728ccfedea04852cc8a
Akhil Saini <akhilsaini678@gmail.com>
2022-01-03 01:00:41 +0530

    Checking if static root working now

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --port=$PORT codeforcestool.wsgi:application 
+web: waitress-serve --port=$PORT codeforcestool.wsgi:application
--- codeforcestool/settings.py
+++ codeforcestool/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
